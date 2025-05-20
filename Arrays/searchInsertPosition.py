30df4dc6e0a9d34c6365a51b4c90ada2f4b83d55
Sattyaj Paul <sattyaj.paul@hotmail.com>
2025-05-20 12:03:05 +0100

    Create searchInsertPosition.py
    binary search to find the index of the solution

---
+++ Arrays/searchInsertPosition.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        #using a binary search strategy
+    
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2 # finding the mid point
+
+            if nums[mid] == target:
+                return mid 
+            elif nums[mid] < target:
+                low = mid + 1 
+            else:
+                high = mid - 1
+
+        return low
+
+    
+       
