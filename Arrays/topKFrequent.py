b561098b3131c481b47f18e8abf4475a040682b6
Sattyaj Paul <sattyaj.paul@hotmail.com>
2025-06-03 12:52:35 +0100

    Update topKFrequent.py

--- Arrays/topKFrequent.py
+++ Arrays/topKFrequent.py
@@ -7,12 +7,14 @@ class Solution(object):
         """
         
 
-
+        #list to add outputs to 
         out = []
 
-
+        #hashmap to store the coutn of the each number
         hmap = {}
 
+
+        #iterate over the list and add new new items or increase the count of items
         for n in nums:
             if n not in hmap:
                 hmap[n] = 1
@@ -20,12 +22,19 @@ class Solution(object):
                 hmap[n] += 1
 
 
+
+        #loop to get the k top frequent nums 
         while k > 0:
             
+            #get the key with max values
             max_key = max(hmap, key=hmap.get)
-            out.append(max_key)
-            del hmap[max_key]
-            k -= 1
 
-        
+            #add the max key to the output list
+            out.append(max_key)
+
+            #delete the max from the map for the next iteration
+            del hmap[max_key]
+            k -= 1 #decrease k by one for the next iteration
+
+        #return the output list
         return out
