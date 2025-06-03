2d94813ca36b865f59789602abdc282e4af3232a
Sattyaj Paul <sattyaj.paul@hotmail.com>
2025-06-03 11:02:51 +0100

    Create findAllNumbersDisappeared.py

---
+++ Arrays/findAllNumbersDisappeared.py
@@ -0,0 +1,45 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+
+        out = []
+        nodup = []
+
+
+        for n in nums:
+            if n not in nodup:
+                nodup.append(n)
+        
+        i = 1
+
+        while i <= len(nums):
+            if i not in nodup:
+                out.append(i)
+
+            i += 1
+        
+        return out
+        
+
+
+        """
+        class Solution(object):
+        def findDisappearedNumbers(self, nums):
+        
+        for n in nums:
+            index = abs(n) - 1
+            if nums[index] > 0:
+                nums[index] *= -1  # mark as seen by making it negative
+
+        out = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                out.append(i + 1)  # i + 1 was not seen
+
+        return out
+
+        """
