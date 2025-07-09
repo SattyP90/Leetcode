42c92be17fd1076932f75eb45af6ff1a184b2b40
Sattyaj Paul <sattyaj.paul@hotmail.com>
2025-07-09 13:04:32 +0100

    Create palindromeNumber.py

---
+++ Math/palindromeNumber.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        digits = [str(d) for d in str(x)]
+
+        right = len(digits) -1 
+        left = 0
+
+        out = True
+
+        while left < right:
+            if digits[left] != digits[right]:
+                return False  
+            left += 1
+            right -= 1
+
+        return True
+
+        return out
+
+            
+
+
+
+        
