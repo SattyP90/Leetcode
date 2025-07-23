3b27ed6bc98b7e5cc7a7c609b11c8371820c7f7e
Sattyaj Paul <sattyaj.paul@hotmail.com>
2025-07-23 12:46:49 +0100

    Create addStrings.py

---
+++ String/addStrings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+
+
+
+
+        out = int(num1) + int (num2)
+        return str(out)
