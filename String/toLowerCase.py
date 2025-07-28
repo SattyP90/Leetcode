c18a7b80d3f199e0501ffedeb96a10a4f5835f68
Sattyaj Paul <sattyaj.paul@hotmail.com>
2025-07-28 21:29:31 +0100

    Create toLowerCase.py

---
+++ String/toLowerCase.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+
+        out = s.lower()
+
+        return out
